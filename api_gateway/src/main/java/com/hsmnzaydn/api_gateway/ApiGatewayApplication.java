0a356a207bb27ae769785f6fb4a2698017cb8e82
Serkan <serkan@basefy.com>
2020-01-07 18:28:05 +0300

    Add docker compose

--- api_gateway/src/main/java/com/hsmnzaydn/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/hsmnzaydn/api_gateway/ApiGatewayApplication.java
@@ -3,10 +3,12 @@ package com.hsmnzaydn.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
--- api_gateway/src/test/java/com/hsmnzaydn/api_gateway/ApiGatewayApplicationTests.java
+++ api_gateway/src/test/java/com/hsmnzaydn/api_gateway/ApiGatewayApplicationTests.java
@@ -1,12 +1,9 @@
 package com.hsmnzaydn.api_gateway;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
