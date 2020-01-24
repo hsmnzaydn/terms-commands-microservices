7795f74058a31f6de8a3eecb2df7555aec76a311
Serkan <serkan@basefy.com>
2020-01-24 19:35:49 +0300

    add docker-stack.yaml for docker swarm

---
+++ update_services.sh
@@ -0,0 +1,5 @@
+docker service update term_api_gateway
+docker service update term_category
+docker service update term_command
+docker service update term_user
+docker service update term_discovery
